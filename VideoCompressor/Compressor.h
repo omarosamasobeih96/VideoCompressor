0077608757f2e5024cd327e4a156587f0fda419a
omarosamasobeih96 <omarosamasobeih@yahoo.com>
2019-03-16 03:02:38 +0200

    suggested compression outlines

--- VideoCompressor/Compressor.h
+++ VideoCompressor/Compressor.h
@@ -1,6 +1,17 @@
 #pragma once
 class Compressor
 {
+	/*
+		1- pattern search
+		2- run volume encoding
+		3- huffman encoding
+		4- difference vs absolute values
+		5- value compression
+		6- Wael's numeric compression
+		7- segmentation
+		8- pixel-frame dropping
+		9- color approximation (K-means)
+	*/
 public:
 	Compressor();
 	~Compressor();
